6b0a84f86a999466056d00e8f06ffd0e07788c83
AllenCheng <allenczh@students.cs.ubc.ca>
2024-02-25 20:52:27 -0800

    apt -y added & show result

--- Ubuntu/docker_install.sh
+++ Ubuntu/docker_install.sh
@@ -14,25 +14,27 @@ echo \
 sudo apt-get update
 
 # Install the Docker packages: 
-sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
 # start docker service and enable docker as log in
 sudo systemctl enable --now docker.service
 sudo systemctl enable --now containerd.service
 
 # Verify that the Docker Engine installation is successful by running the hello-world image.
-sudo docker run -rm hello-world
-
-if echo "$line" | grep -E "Hello from Docker!"; then
-  echo "docker install successfully!"
+if sudo docker run --rm hello-world | grep -q "Hello from Docker!"; then
+  if docker compose version | grep -q "Docker Compose version"; then
+    # add current user to docker group to run docker command without sudo
+    # Create the docker group.
+    sudo groupadd docker
+    # Add your user to the docker group.
+    sudo usermod -aG docker $USER
+    # activate the changes to groups
+    newgrp docker
+    # Verify that you can run docker commands without sudo.
+    docker run --rm hello-world
+  else
+    echo "Docker install successfully! Docker compose does not!"
+    echo "check docker docs: https://docs.docker.com/compose/install/"
 else
-  echo "docker install fail!"
-fi
-
-docker compose version
-
-if echo "$line" | grep -E "Docker Compose version"; then
-  echo "docker-compose install successfully!"
-else
-  echo "docker-compose install fail!"
-fi
+  echo "Docker install fail!"
+  echo "check docker docs: "https://docs.docker.com/engine/install/ubuntu/#install-using-the-repository"
